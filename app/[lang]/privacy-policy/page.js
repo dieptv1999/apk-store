4b952c8c4c40dab114e3b4df0315f029dc1f0fe1
dieptv1999 <diep.tv1999@gmail.com>
2023-12-24 23:07:12 +0700

    init

--- app/[lang]/privacy-policy/page.js
+++ app/[lang]/privacy-policy/page.js
@@ -11,14 +11,19 @@ export default async function AboutUs() {
       <div className={'max-w-screen-xl w-full px-[10px] flex flex-col space-y-6'}>
         <div className={'text-black font-bold tracking-wide text-3xl'}>Privacy & Policy</div>
         <div>
-          Effective as of Dec 24, 2023
+          <div>Effective as of Dec 24, 2023</div>
 
-          At Apk Store, accessible from https://apkstore.com, one of our main priorities is the privacy of our visitors.
-          This Privacy Policy document contains types of information that is collected and recorded by apk store and how
-          we use it.
+          <div>At Apk Store, accessible from https://apkstore.com, one of our main priorities is the privacy of our
+            visitors.
+            This Privacy Policy document contains types of information that is collected and recorded by apk store and
+            how
+            we use it.
+          </div>
 
-          If you have additional questions or require more information about our Privacy Policy, do not hesitate to
-          contact us.
+          <div>
+            If you have additional questions or require more information about our Privacy Policy, do not hesitate to
+            contact us.
+          </div>
         </div>
         <div className={'text-black font-bold tracking-wide text-2xl'}>Log Files</div>
         <div>
@@ -39,35 +44,49 @@ export default async function AboutUs() {
         </div>
         <div className={'text-black font-bold tracking-wide text-2xl'}>Our Advertising Partners</div>
         <div>
-          Some of advertisers on our site may use cookies and web beacons. Our advertising partners are listed below.
-          Each of our advertising partners has their own Privacy Policy for their policies on user data. For easier
-          access, we hyperlinked to their Privacy Policies below.
+          <div>
+            Some of advertisers on our site may use cookies and web beacons. Our advertising partners are listed below.
+            Each of our advertising partners has their own Privacy Policy for their policies on user data. For easier
+            access, we hyperlinked to their Privacy Policies below.
+          </div>
 
-          Google
+          <div>Google</div>
 
           https://policies.google.com/technologies/ads
         </div>
         <div className={'text-black font-bold tracking-wide text-2xl'}>Privacy Policies</div>
         <div>
-          You may consult this list to find the Privacy Policy for each of the advertising partners of apk store.
+          <p>
+            You may consult this list to find the Privacy Policy for each of the advertising partners of apk store.
+          </p>
 
-          Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used
-          in their respective advertisements and links that appear on apk store, which are sent directly to users'
-          browser. They automatically receive your IP address when this occurs. These technologies are used to measure
-          the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on
-          websites that you visit.
-
-          Note that Apk store has no access to or control over these cookies that are used by third-party advertisers.
+          <div>
+            Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are
+            used
+            in their respective advertisements and links that appear on apk store, which are sent directly to users'
+            browser. They automatically receive your IP address when this occurs. These technologies are used to measure
+            the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see
+            on
+            websites that you visit.
+          </div>
+          <div>
+            Note that Apk store has no access to or control over these cookies that are used by third-party advertisers.
+          </div>
         </div>
         <div className={'text-black font-bold tracking-wide text-2xl'}>Third Party Privacy Policies</div>
         <div>
-          Apk store's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to
-          consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may
-          include their practices and instructions about how to opt-out of certain options.
-
-          You can choose to disable cookies through your individual browser options. To know more detailed information
-          about cookie management with specific web browsers, it can be found at the browsers' respective websites. What
-          Are Cookies?
+          <div>
+            Apk store's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to
+            consult the respective Privacy Policies of these third-party ad servers for more detailed information. It
+            may
+            include their practices and instructions about how to opt-out of certain options.
+          </div>
+          <div>
+            You can choose to disable cookies through your individual browser options. To know more detailed information
+            about cookie management with specific web browsers, it can be found at the browsers' respective websites.
+            What
+            Are Cookies?
+          </div>
         </div>
         <div className={'text-black font-bold tracking-wide text-2xl'}>Online Privacy Policy Only</div>
         <div>
--- app/[lang]/terms/page.js
+++ app/[lang]/terms/page.js
@@ -11,38 +11,172 @@ export default async function AboutUs() {
       <div className={'max-w-screen-xl w-full px-[10px] flex flex-col space-y-6'}>
         <div className={'text-black font-bold tracking-wide text-3xl'}>Terms & Condition</div>
         <div>
-          Effective as of May 25, 2018
+          <div>
+            Effective as of Dec 24, 2023
+          </div>
 
-          Our updated Privacy Policy explains what information we collect, how we use it, and how we protect it. We also
-          explain your rights regarding our use of your personal information, including how to manage your privacy and
-          cookies settings, access the information we have about you, and delete your account, under the privacy laws
-          including but not limited to California Consumer Privacy Act of 2018 (“CCPA”) and the General Data Protection
-          Regulation (GDPR).
+          <div className={'my-4 text-xl font-semibold'}>Welcome to apk store!</div>
 
-          APKPure is committed to protecting consumer privacy online. We believe that greater protection of personal
-          privacy on the Web will not only protect our users, but also increase users confidence and ultimately their
-          participation in online activities. The purpose of our policy is to inform you about the types of information
-          we gather about you when you visit our site, how we may use that information, whether we disclose it to
-          anyone, and the rights you have regarding our use of the information. APKPure.com strives to offer its
-          visitors the many advantages of Internet technology and to provide an interactive and personalized experience.
+          <div>
+            By accessing this website we assume you accept these terms and conditions. Do not continue to use apk store
+            if you do not agree to take all of the terms and conditions stated on this page.
+          </div>
+          <div>
+            The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and
+            all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to
+            the Company's terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company.
+            "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer,
+            acceptance and consideration of payment necessary to undertake the process of our assistance to the Client
+            in the most appropriate manner for the express purpose of meeting the Client's needs in respect of provision
+            of the Company's stated services, in accordance with and subject to, prevailing law of af. Any use of the
+            above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as
+            interchangeable and therefore as referring to same.
+          </div>
         </div>
-        <div className={'text-black font-bold tracking-wide text-2xl'}>Your Consent</div>
+        <div className={'text-black font-bold tracking-wide text-2xl'}>Cookies</div>
         <div>
-          BY USING APKPure SERVICES, INSTALLING AND/OR RUNNING ON YOUR MOBILE DEVICE OR BROWSER, ENTERING INTO,
-          CONNECTING TO, ACCESSING AND/OR USING THE APP, YOU AGREE TO THE TERMS AND CONDITIONS SET FORTH IN THIS PRIVACY
-          POLICY, INCLUDING TO THE POSSIBLE COLLECTION AND PROCESSING OF YOUR PERSONAL INFORMATION. PLEASE NOTE: IF YOU
-          OR, AS APPLICABLE, YOUR LEGAL GUARDIAN, DISAGREE TO ANY TERM PROVIDED HEREIN, YOU MUST NOT INSTALL, ACCESS
-          AND/OR USE THE APP, APKPure OTHER SERVICES AND YOU ARE REQUESTED TO PROMPTLY ERASE THE APP FROM YOUR MOBILE
-          DEVICE OR STOP USING OUR APP/WEBSITE THROUGH YOUR BROWSER AND DO NOT ENTER TO, CONNECT TO, ACCESS OR USE ANY
-          OF OUR SERVICES RELATED TO THE APP.
+          <p>We employ the use of cookies. By accessing apk store, you agreed to use cookies in agreement with the Apk
+            Store's Privacy Policy. </p>
+
+          <p>Most interactive websites use cookies to let us retrieve the user's details for each visit. Cookies are
+            used by our website to enable the functionality of certain areas to make it easier for people visiting our
+            website. Some of our affiliate/advertising partners may also use cookies.</p>
         </div>
-        <div className={'text-black font-bold tracking-wide text-2xl'}>Important Notice</div>
+        <div className={'text-black font-bold tracking-wide text-2xl'}>License</div>
         <div>
-          APKPure.com is NOT associated or affiliated with Google, Google Play or Android in any way. Android is a
-          trademark of Google Inc. All the apps & games are property and trademark of their respective developer or
-          publisher and for HOME or PERSONAL use ONLY. Please be aware that APKPure.com ONLY SHARE THE ORIGINAL APK FILE
-          FOR FREE APPS. ALL THE APK FILE IS THE SAME AS IN GOOGLE PLAY WITHOUT ANY CHEAT, UNLIMITED GOLD PATCH OR ANY
-          OTHER MODIFICATIONS.
+          <p>Unless otherwise stated, Apk Store and/or its licensors own the intellectual property rights for all
+            material on apk store. All intellectual property rights are reserved. You may access this from apk store for
+            your own personal use subjected to restrictions set in these terms and conditions.</p>
+
+          <p>You must not:</p>
+          <ul className={'list-disc pl-5 py-3'}>
+            <li>Republish material from apk store</li>
+            <li>Sell, rent or sub-license material from apk store</li>
+            <li>Reproduce, duplicate or copy material from apk store</li>
+            <li>Redistribute content from apk store</li>
+          </ul>
+
+          <p>This Agreement shall begin on the date hereof. Our Terms and Conditions were created with the help of
+            the <a href="https://www.termsandconditionsgenerator.com/">Free Terms and Conditions Generator</a>.</p>
+
+          <p>Parts of this website offer an opportunity for users to post and exchange opinions and information in
+            certain areas of the website. Apk Store does not filter, edit, publish or review Comments prior to their
+            presence on the website. Comments do not reflect the views and opinions of Apk Store,its agents and/or
+            affiliates. Comments reflect the views and opinions of the person who post their views and opinions. To the
+            extent permitted by applicable laws, Apk Store shall not be liable for the Comments or for any liability,
+            damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of
+            the Comments on this website.</p>
+
+          <p>Apk Store reserves the right to monitor all Comments and to remove any Comments which can be considered
+            inappropriate, offensive or causes breach of these Terms and Conditions.</p>
+
+          <p>You warrant and represent that:</p>
+
+          <ul className={'list-disc pl-5 py-3'}>
+            <li>You are entitled to post the Comments on our website and have all necessary licenses and consents to do
+              so;
+            </li>
+            <li>The Comments do not invade any intellectual property right, including without limitation copyright,
+              patent or trademark of any third party;
+            </li>
+            <li>The Comments do not contain any defamatory, libelous, offensive, indecent or otherwise unlawful material
+              which is an invasion of privacy
+            </li>
+            <li>The Comments will not be used to solicit or promote business or custom or present commercial activities
+              or unlawful activity.
+            </li>
+          </ul>
+
+          <p>You hereby grant Apk Store a non-exclusive license to use, reproduce, edit and authorize others to use,
+            reproduce and edit any of your Comments in any and all forms, formats or media.</p>
+
+        </div>
+        <div className={'text-black font-bold tracking-wide text-2xl'}>Hyperlinking to our Content</div>
+        <div>
+          <p>The following organizations may link to our Website without prior written approval:</p>
+
+          <ul className={'list-disc pl-5 py-3'}>
+            <li>Government agencies;</li>
+            <li>Search engines;</li>
+            <li>News organizations;</li>
+            <li>Online directory distributors may link to our Website in the same manner as they hyperlink to the
+              Websites of other listed businesses; and
+            </li>
+            <li>System wide Accredited Businesses except soliciting non-profit organizations, charity shopping malls,
+              and charity fundraising groups which may not hyperlink to our Web site.
+            </li>
+          </ul>
+
+          <p>These organizations may link to our home page, to publications or to other Website information so long as
+            the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval
+            of the linking party and its products and/or services; and (c) fits within the context of the linking
+            party's site.</p>
+
+          <p>We may consider and approve other link requests from the following types of organizations:</p>
+
+          <ul className={'list-disc pl-5 py-3'}>
+            <li>commonly-known consumer and/or business information sources;</li>
+            <li>dot.com community sites;</li>
+            <li>associations or other groups representing charities;</li>
+            <li>online directory distributors;</li>
+            <li>internet portals;</li>
+            <li>accounting, law and consulting firms; and</li>
+            <li>educational institutions and trade associations.</li>
+          </ul>
+
+          <p>We will approve link requests from these organizations if we decide that: (a) the link would not make us
+            look unfavorably to ourselves or to our accredited businesses; (b) the organization does not have any
+            negative records with us; (c) the benefit to us from the visibility of the hyperlink compensates the absence
+            of Apk Store; and (d) the link is in the context of general resource information.</p>
+
+          <p>These organizations may link to our home page so long as the link: (a) is not in any way deceptive; (b)
+            does not falsely imply sponsorship, endorsement or approval of the linking party and its products or
+            services; and (c) fits within the context of the linking party's site.</p>
+
+          <p>If you are one of the organizations listed in paragraph 2 above and are interested in linking to our
+            website, you must inform us by sending an e-mail to Apk Store. Please include your name, your organization
+            name, contact information as well as the URL of your site, a list of any URLs from which you intend to link
+            to our Website, and a list of the URLs on our site to which you would like to link. Wait 2-3 weeks for a
+            response.</p>
+
+          <p>Approved organizations may hyperlink to our Website as follows:</p>
+
+          <ul className={'list-disc pl-5 py-3'}>
+            <li>By use of our corporate name; or</li>
+            <li>By use of the uniform resource locator being linked to; or</li>
+            <li>By use of any other description of our Website being linked to that makes sense within the context and
+              format of content on the linking party's site.
+            </li>
+          </ul>
+
+          <p>No use of Apk Store's logo or other artwork will be allowed for linking absent a trademark license
+            agreement.</p>
+        </div>
+        <div className={'text-black font-bold tracking-wide text-2xl'}>Content Liability</div>
+        <div>
+          We shall not be hold responsible for any content that appears on your Website. You agree to protect and defend
+          us against all claims that is rising on your Website. No link(s) should appear on any Website that may be
+          interpreted as libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the
+          infringement or other violation of, any third party rights.
+        </div>
+        <div className={'text-black font-bold tracking-wide text-2xl'}>Disclaimer</div>
+        <div>
+          <p>To the maximum extent permitted by applicable law, we exclude all representations, warranties and
+            conditions relating to our website and the use of this website. Nothing in this disclaimer will:</p>
+
+          <ul className={'list-disc pl-5 py-3'}>
+            <li>limit or exclude our or your liability for death or personal injury;</li>
+            <li>limit or exclude our or your liability for fraud or fraudulent misrepresentation;</li>
+            <li>limit any of our or your liabilities in any way that is not permitted under applicable law; or</li>
+            <li>exclude any of our or your liabilities that may not be excluded under applicable law.</li>
+          </ul>
+
+          <p>The limitations and prohibitions of liability set in this Section and elsewhere in this disclaimer: (a) are
+            subject to the preceding paragraph; and (b) govern all liabilities arising under the disclaimer, including
+            liabilities arising in contract, in tort and for breach of statutory duty.</p>
+
+          <p>As long as the website and the information and services on the website are provided free of charge, we will
+            not be liable for any loss or damage of any nature.</p>
         </div>
         <div className={'text-black font-bold tracking-wide text-2xl'}>Follow APKPure on Social Media</div>
         <div className={'flex flex-col'}>
